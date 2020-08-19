eb94e3e39b09444c1e140bf920183fde9b896c97
Kaliary Cisne de Sousa da Silva <kaliarycisne@gmail.com>
2020-08-19 00:00:56 -0300

    App

--- src/App.js
+++ src/App.js
@@ -3,12 +3,9 @@ import React from 'react';
 import { Global } from './styles/Global';
 import Routes from './Routes';
 
-import Book from './pages/Book';
-
 function App() {
   return (
     <>
-      <Book />
       <Routes />
       <Global />
     </>
